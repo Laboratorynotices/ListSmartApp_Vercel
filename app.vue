1b3f92feaa2714ff3abee586837ab3dd0cc88cf9
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-02-13 22:09:49 +0100

    feat(shopping-store): добавлен черновик хранилища и тест в App.vue
    - Создан файл `stores/useShoppingStore.ts` с черновиком хранилища
    - В `App.vue` добавлен код для тестирования хранилища Pinia

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { useShoppingStore } from "@/stores/useShoppingStore";
+
+// Получаем доступ к хранилищу
+const shoppingStore = useShoppingStore();
+
+// Проверим содержимое
+console.log(shoppingStore.items);
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
