e6917684744c80566acc1a20c30d952ed71e78ef
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-02-13 16:03:45 +0100

    config(vuefire): добавлен vuefire с настройками авторизации и CSR-доступа

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: true,
+  modules: ["nuxt-vuefire"],
+  // Настройки модуля VueFire
+  vuefire: {
+    // Включение аутентификации
+    auth: {
+      enabled: true,
+      sessionCookie: true,
+    },
+    config: {
+      // Настройки для Firebase
+      apiKey: process.env.GOOGLE_FIREBASE_API_KEY,
+      authDomain: process.env.GOOGLE_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.GOOGLE_FIREBASE_PROJECT_ID,
+      appId: process.env.GOOGLE_FIREBASE_CONFIG_APP_ID,
+    },
+  },
 });
