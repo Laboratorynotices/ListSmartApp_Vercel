361e05c061a8112fa29e949a4435052b4cca0baa
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-03-05 21:22:40 +0100

    Используем утилиту для получения ID пользователя, чтобы вывести его список покупок.

--- server/api/shopping-list/index.ts
+++ server/api/shopping-list/index.ts
@@ -1,15 +1,23 @@
 import { defineEventHandler } from "h3";
 import { collection, getDocs } from "firebase/firestore";
 import { getDb } from "@/server/utils/firebase";
+import getUserUid from "@/server/utils/getUserUid";
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   try {
+    // Получаем ID пользователя
+    const uid = await getUserUid(event);
+
+    if (!uid) {
+      return { error: "Unauthorized" };
+    }
+
     // Получаем Firestore
     const db = getDb();
 
     const shoppingItemsCollection = collection(
       db,
-      "users/B1rXCvfUcSWwp9YlR07XcboQEHj2/shoppingItems"
+      `users/${uid}/shoppingItems`
     );
     // Читаем данные из Firestore
     const snapshot = await getDocs(shoppingItemsCollection);
---
+++ server/utils/getUserUid.ts
@@ -0,0 +1,23 @@
+import { adminAuth } from "@/server/utils/firebaseAdmin";
+
+export default async function getUserUid(event: any): Promise<string | null> {
+  // Получаем конфигурацию приложения
+  const config = useRuntimeConfig();
+  // Получаем имя куки
+  const cookieName = config.private.sessionCookieName;
+  // Получаем куки из запроса
+  const cookie = getCookie(event, cookieName);
+
+  if (!cookie) return null;
+
+  try {
+    // Проверяем сессию и получаем декодированные данные пользователя
+    const decodedToken = await adminAuth.verifySessionCookie(cookie, true);
+
+    // Возвращаем ID пользователя
+    return decodedToken.uid;
+  } catch (error) {
+    console.error("Session verification failed:", error);
+    return null;
+  }
+}
